616375e6c3a1c661dd75bc83f53824486b701ae6
JangidLalith <jangidlalith569@gmail.com>
2025-01-18 11:43:12 +0530

    lucky

--- src/main/java/com/example/demo/controller/MyUserController.java
+++ src/main/java/com/example/demo/controller/MyUserController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.MyUser;
 import com.example.demo.service.MyUserService;
 
 @RestController
@@ -27,5 +32,25 @@ public class MyUserController {
 		
 		return res;
 	}
-
+	
+	
+	@PostMapping("/varun-motors/user/add")
+	public String addNewUser(@RequestBody MyUser myUser){
+		
+		System.out.println("data---->"+myUser);
+		
+		String res = myUserSer.addNewUserRecord(myUser);
+		
+		return res;
+	}
+	
+	@PostMapping("/varun-motors/user/add/all")
+	public String addAllNewUsers(@RequestBody List<MyUser> usersList){
+		
+		System.out.println("data---->"+usersList);
+		
+		String res = myUserSer.addAllNewUserRecords(usersList);
+		
+		return res;
+	}
 }
